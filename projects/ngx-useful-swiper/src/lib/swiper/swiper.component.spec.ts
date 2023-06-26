290e07b159186bea5a09b851a9aafd22dc10a106
jyotikanta <sanjay.row.com@gmail.com>
2023-06-26 19:06:39 +0530

    moved to 11

--- projects/ngx-useful-swiper/src/lib/swiper/swiper.component.spec.ts
+++ projects/ngx-useful-swiper/src/lib/swiper/swiper.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { SwiperComponent } from './swiper.component';
 
@@ -6,7 +6,7 @@ describe('SwiperComponent', () => {
   let component: SwiperComponent;
   let fixture: ComponentFixture<SwiperComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ SwiperComponent ]
     })
